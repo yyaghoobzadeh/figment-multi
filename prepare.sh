747056a63205288bd058eb04b3351742da20de93
Yadollah Yaghoobzadeh <yadollah@cis.lmu.de>
2017-03-08 14:46:52 +0100

    .

--- prepare.sh
+++ prepare.sh
@@ -3,6 +3,7 @@ wget http://cistern.cis.lmu.de/figment2/embeddings/skip,200dim.txt
 wget http://cistern.cis.lmu.de/figment2/embeddings/sskip,200dim.txt
 wget http://cistern.cis.lmu.de/figment2/embeddings/fasttext,200dim.txt
 
+mkdir skip
 cd skip
 cp ../datasets/* .
 mv ../skip,200dim.txt .
@@ -11,6 +12,7 @@ python ../../src/classification/nn/gm/make_fuel_dataset.py config &
 wait
 rm Etrain.names Edev.names Etest.names types
 
+mkdir sskip
 cd sskip
 cp ../datasets/* .
 mv ../sskip,200dim.txt .
